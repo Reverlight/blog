d5ba98f773cd2f81f815bb2f78ffbe12ca7cabd1
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-04-30 12:32:42 +0300

    Add and view comments in post detail

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
+from blog.models import Comment
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100, required=True)
@@ -12,3 +14,9 @@ class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ['username', 'password1', 'password2']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, CreateView, View, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, View
 
-from .models import Post
-from .forms import LoginForm, SignupForm
+from .models import Post, Comment
+from .forms import LoginForm, SignupForm, CommentForm
 
 
 class PostListView(ListView):
@@ -18,9 +19,41 @@ class PostCreateView(CreateView):
     template_name = 'blog/post_create.html'
 
 
-class PostDetailView(DetailView):
-    model = Post
+class PostDetailView(LoginRequiredMixin, View):
     template_name = 'blog/post_detail.html'
+    post_model = Post
+    comment_model = Comment
+    comment_form_class = CommentForm
+
+    def get(self, request, pk):
+        post = get_object_or_404(self.post_model, pk=pk)
+        comments = self.comment_model.objects.filter(post=post)
+        comment_form = self.comment_form_class()
+
+        context = {
+            'comment_form': comment_form,
+            'comments': comments,
+            'post': post
+        }
+        return render(request, self.template_name, context=context)
+
+    def post(self, request, pk):
+        post = get_object_or_404(self.post_model, pk=pk)
+        comments = self.comment_model.objects.filter(post=post)
+        comment_form = self.comment_form_class(request.POST)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+
+        context = {
+            'comment_form': comment_form,
+            'comments': comments,
+            'post': post
+        }
+        return render(request, self.template_name, context=context)
 
 
 class UserCreateView(View):

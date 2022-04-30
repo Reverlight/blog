016e8a7f508e4fa05f5c020c3796a0c603f875ad
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-04-30 11:55:26 +0300

    Post detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from blog.views import PostListView, PostCreateView, UserCreateView, UserLoginView
+from blog.views import PostListView, PostCreateView, UserCreateView, UserLoginView, PostDetailView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
     path('post/create/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+
     path('user/signup/', UserCreateView.as_view(), name='signup'),
     path('user/login/', UserLoginView.as_view(), name='login')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, CreateView, View
+from django.views.generic import ListView, CreateView, View, DetailView
 
 from .models import Post
 from .forms import LoginForm, SignupForm
@@ -16,6 +16,11 @@ class PostCreateView(CreateView):
     model = Post
     fields = ['theme', 'description', 'image']
     template_name = 'blog/post_create.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
 
 
 class UserCreateView(View):

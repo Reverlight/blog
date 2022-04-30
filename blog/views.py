2bbf31b80c0369437857bdc56f14b1652b1bc9ac
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-04-30 11:19:27 +0300

    User login view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView, CreateView
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView, View
 
-from blog.models import Post, User
+from .models import Post, User
+from .forms import LoginForm
 
 
 class PostListView(ListView):
@@ -19,3 +22,32 @@ class UserCreateView(CreateView):
     model = User
     fields = ['username', 'password']
     template_name = 'blog/user_create.html'
+
+
+class UserLoginView(View):
+    template_name = 'blog/user_login.html'
+    form_class = LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        context = {
+            'form': form
+        }
+        return render(request, self.template_name, context=context)
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password']
+            )
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+
+        context = {
+            'form': form
+        }
+        return render(request, self.template_name, context=context)

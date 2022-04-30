23b8cd8f4ea83ea50c7be81558f1477d4032acff
Artem Jheleznov <52380931+Reverlight@users.noreply.github.com>
2022-04-30 11:43:54 +0300

    Rm author migration

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,6 @@ class Post(models.Model):
     theme = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='uploads/%Y/%m/%d', blank=True, null=True)
-    author = models.ForeignKey('User', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.theme
